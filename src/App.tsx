e48ba6174dc978da9ee99b9ecf5b3d0e6eadf836
sarswatPreeti <saraswatpreeti.developer@gmail.com>
2025-11-18 23:10:01 +0530

    Add main application entry files

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Today from "./pages/Today";
+import Projects from "./pages/Projects";
+import AllTodos from "./pages/AllTodos";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Today />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/all" element={<AllTodos />} />
+          <Route path="/important" element={<Today />} />
+          <Route path="/personal" element={<Today />} />
+          <Route path="/completed" element={<Today />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
